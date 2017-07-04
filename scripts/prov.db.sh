12e93202dfcde5e523de62ec253b7c8c2c5509e0
Rick Teh <rickteh@melbpc.org.au>
2017-07-04 21:03:45 +1000

    Rejigged so database password is set randomly

--- scripts/prov.db.sh
+++ scripts/prov.db.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-# Install MySQL on Ubuntu
+# Install MySQL on Ubuntu, set a root password and secure the installation
 # Called by vagrant up or vagrant provision
 
 export DEBIAN_FRONTEND="noninteractive"
 
-debconf-set-selections <<< "mysql-server-5.6 mysql-server/root_password password mytemppw"
-debconf-set-selections <<< "mysql-server-5.6 mysql-server/root_password_again password mytemppw"
+PW=$(openssl rand -base64 12)
 
-apt-get install -y mysql-server
+cat<<End_PW>/root/.my.cnf
+[client]
+user=root
+password=$PW
+End_PW
+chmod 0400 /root/.my.cnf
 
+apt-get install debconf-utils
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $PW"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PW"
+
+apt-get install -y mysql-server 
+
+# I suppose if I'm answering no to everything I needn't bother 
+# echo -e "\n\n\n\n\n\n" | /usr/bin/mysql_secure_installation
 
--- scripts/prov.web.sh
+++ scripts/prov.web.sh
@@ -4,7 +4,7 @@
 # Called by vagrant up or vagrant provision
 
 apt-get update
-apt-get install -y apache2 php libapache2-mod-php php-mcrypt php-mysql debconf-utils
+apt-get install -y apache2 php libapache2-mod-php php-mcrypt php-mysql
 usermod -a -G www-data ubuntu
 echo '<?php phpinfo(); ?>' > /var/www/html/phpinfo.php
 chown -R ubuntu:www-data /var/www
