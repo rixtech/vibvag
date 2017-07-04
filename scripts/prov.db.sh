ddb28e1702dd405f6c1cadd4e4798538d1a619a3
Rick Teh <rickteh@melbpc.org.au>
2017-07-04 22:56:33 +1000

    set MySQL bind address to local IP. Create user for webapp

--- scripts/prov.db.sh
+++ scripts/prov.db.sh
@@ -23,3 +23,24 @@ apt-get install -y mysql-server
 # I suppose if I'm answering no to everything I needn't bother 
 # echo -e "\n\n\n\n\n\n" | /usr/bin/mysql_secure_installation
 
+# Tell MySQL to listen on its private IP (restart needed to enable)
+pIP=`curl http://169.254.169.254/latest/meta-data/local-ipv4 2>/dev/null`
+sed -i -e 's/^bind-address.*/bind-address = '$pIP'/' /etc/mysql/mysql.conf.d/mysqld.cnf
+systemctl restart mysql.service
+
+# Add a webservice user and test database
+mysql<<EndSQL
+USE mysql;
+CREATE DATABASE testdb;
+CREATE USER webapp IDENTIFIED BY 'webappPW';
+GRANT ALL ON testdb.* TO 'webapp'@'%';
+FLUSH PRIVILEGES;
+USE testdb;
+CREATE table test (c CHAR(255));
+INSERT INTO test (c) VALUES ("hello world");
+EndSQL
+
+
+
+
+
